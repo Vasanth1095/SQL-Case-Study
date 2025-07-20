53a3fcad606f05bd7b820d20596b5fbc1b7ab166
Vasanth1095 <ashwin061998@gmail.com>
2025-07-20 10:14:28 +0530

    Delete profit_query.sql

--- profit_query.sql
+++
@@ -1,7 +0,0 @@
-select mm_yy,fiscal_year,ns.product_code,customer_code,sold_quantity,
-gross_sales,net_sales,round((manufacturing_cost*sold_quantity),2) AS total_manufacturing_cost,
-round(gross_sales-(manufacturing_cost*sold_quantity),2) AS Profit
-from net_sales_tb as ns
-join fact_manufacturing_cost as mc
-	on mc.product_code = ns.product_code AND
-		mc.cost_year = ns.fiscal_year
