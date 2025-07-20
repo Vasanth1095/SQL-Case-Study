9b2a0ca1ca77e48d7f19de7d1849d1fd6a17f3b3
Vasanth1095 <ashwin061998@gmail.com>
2025-07-20 10:13:41 +0530

    Delete Query_3.sql

--- Query_3.sql
+++
@@ -1,8 +0,0 @@
--- Provide a report with all the unique product counts for each segment --
-SELECT 
-	segment, COUNT(product_code) AS product_count,
-    ROUND(COUNT(product_code)*100/SUM(COUNT(product_code)) OVER(),2) AS Percentage_contribution
-FROM dim_product
-GROUP BY segment
-ORDER BY product_count DESC
- 
